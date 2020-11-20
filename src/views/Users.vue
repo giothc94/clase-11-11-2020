5970953513ebc16216d1ab5a7778e1083205631b
Geovanny G. Arguello <gio666nb@gmail.com>
2020-11-20 10:41:34 -0500

    Render users

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="about">
     <h1>Lista de usuarios</h1>
+    <div>
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Edad</th>
+        </thead>
+        <tr v-for="user in users" :key="user.id_users">
+          <td>{{ user.id_users }}</td>
+          <td>{{ user.first_name }}</td>
+          <td>{{ user.lastname }}</td>
+          <td>{{ user.age }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 import UsersService from "@/services/users";
 export default {
+  data(){
+    return {
+      users: []
+    }
+  },
   mounted(){
     this.getData()
   },
@@ -14,7 +35,8 @@ export default {
     getData(){
       UsersService.getUsers()
         .then((results)=>{
-          console.log(results)
+          this.users = results.data
+          console.log(this.users)
         })
         .catch((error)=>{
           console.error(error)
